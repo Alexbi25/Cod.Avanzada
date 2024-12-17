61e6062d2b7be2235d9e53723bd4cbafca9c08e9
Alexbi25 <alexa_gomez@euneiz.com>
2024-12-17 08:56:20 +0100

    Add pedir numeros al usuario ademas de guardar los
    numeros en lista

--- Solucion/Codificacion-Numeros/Program.cs
+++ Solucion/Codificacion-Numeros/Program.cs
@@ -4,6 +4,16 @@ List<int> numeros = new List<int>();
 //Cantidad de numeros a pedir
 int cantidad = 5;
 //Pedir numeros al usuario 1 en 1 
+for (int i = 0; i < cantidad; i++)
+{
+    Console.WriteLine($"Introduce el número {i + 1}: ");
+    int numero;
+
+    //en caso de no ser numero error
+    while (¡int.TryParse
+}
+//Guardar los numeros en lista
+numeros.Add(numeros);
 //Mostrar la lista generada en la consola
 //Ornar lal lista de menor a mayor 
 // Mostrar por consola la lista ordenada
