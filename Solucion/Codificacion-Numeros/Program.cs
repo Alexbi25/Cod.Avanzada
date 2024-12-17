9aef9de5229d3bd7ffb933a9d4826c94ddb5615b
Alexbi25 <alexa_gomez@euneiz.com>
2024-12-17 08:48:29 +0100

    add crear lista vacia/ cantidad de numeros de lista

--- Solucion/Codificacion-Numeros/Program.cs
+++ Solucion/Codificacion-Numeros/Program.cs
@@ -1,5 +1,9 @@
-﻿//Pedir numeros al usuario 1 en 1 
+﻿
 //Crear lista vacia para almacenar los numeros en una lista
+List<int> numeros = new List<int>();
+//Cantidad de numeros a pedir
+int cantidad = 5;
+//Pedir numeros al usuario 1 en 1 
 //Mostrar la lista generada en la consola
 //Ornar lal lista de menor a mayor 
 // Mostrar por consola la lista ordenada
