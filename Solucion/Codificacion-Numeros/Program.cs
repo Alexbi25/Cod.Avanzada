230bab9b712d36e99dbb660b16e2e314c2f00815
Alexbi25 <alexa_gomez@euneiz.com>
2024-12-17 08:37:41 +0100

    Add pseudocodigo

--- Solucion/Codificacion-Numeros/Program.cs
+++ Solucion/Codificacion-Numeros/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Pedir numeros al usuario 1 en 1 
+//Crear lista vacia para almacenar los numeros en una lista
+//Mostrar la lista generada en la consola
+//Ornar lal lista de menor a mayor 
+// Mostrar por consola la lista ordenada
